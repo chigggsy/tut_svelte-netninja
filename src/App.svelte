1146c95802cd079bcbec0f4cfc6950ef4f330d7e
Chinmay <signups@chinmay.design>
2024-02-16 02:17:13 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	let name = 'Yoshi';
+	let beltColor = 'black';
+
+	const handleClick = () => {
+		beltColor = 'orange';
+	}
+
+	const handleInput = (e) => {
+		beltColor = e.target.value;
+	}
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p style="color: {beltColor};">{beltColor} belt</p>
+	<!-- <input type="text" on:input={handleInput} value={beltColor}> -->
+	<input type="text" bind:value={beltColor}>
+	<button on:click={handleClick}>Update Belt Colour</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
